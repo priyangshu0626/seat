d62d769f8bd9b0a0e5edf88a3e2629b4b5d61ded
Priyangshu Chattopadhyay <bsmpp25priyangshuc@iimsambalpur.ac.in>
2026-08-03 15:30:34 +0530

    Use fixed clean asset names assets/app.js and assets/style.css for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/seat/',
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/chunk.js',
+        assetFileNames: 'assets/style.[ext]',
+      }
+    }
+  }
 })
